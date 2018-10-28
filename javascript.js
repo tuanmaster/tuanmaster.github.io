d6595da9890696a7ebd1ffb49861c9c8dc265c94
Tuan Pham <tuanphamhauik11@gmail.com>
2018-10-28 13:05:23 +0700

    hoan thien html

---
+++ javascript.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+	var traiqua = document.querySelector(".header-title__desc");
+	var trenxuong = document.querySelector(".header-imges__img");
+	var phaiqua = document.querySelector(".header-title__button");
+	traiqua.classList.add('traiqua');
+	phaiqua.classList.add('phaiqua');
+	trenxuong.classList.add('trenxuong');
+
+	//slide
+	{
+		var vtslide;
+		var slidehientai = document.querySelector('header-slide1');
+		for (var vtslide = 0; slidehientai = slidehientai.previousElementSibling; vtslide++) {
+		}
+		console.log(vtslide);
+	}
+	// bắt sự kienj cuộc chuột
+	window.addEventListener('scroll',function(){
+		// if(window.pageYOffset >= 0){
+	}
+	)
+},false)
